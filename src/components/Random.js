5a9e8036b908470a4a271863b42f3c92c5261a59
mightyjimmy9 <feliperli@hotmail.com>
2019-12-02 08:52:20 -0300

    Randomgif button (#6)
    * trigger api on button click
    
    * update gitignore

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import credential from "../credentials";
 
 const RandomComponent = styled.div`
   background-color: #1d2c4e;
@@ -10,14 +11,6 @@ const RandomComponent = styled.div`
   margin: 10px;
 `;
 
-const Header = styled.h3`
-  color: #732e85;
-  margin: 0;
-  padding: 0;
-  font-size: 1.5rem;
-  font-family: "roboto";
-`;
-
 const RandomButton = styled.button`
   color: purple;
   background-color: white;
@@ -26,11 +19,35 @@ const RandomButton = styled.button`
 `;
 
 export default class Random extends Component {
+  state = {
+    embedGif: ""
+  };
+
+  getGif = () => {
+    const axios = require("axios");
+    axios
+      .get("https://api.giphy.com/v1/gifs/random", {
+        params: {
+          api_key: credential
+        }
+      })
+      .then(response => {
+        this.setState({
+          embedGif: response.data.data.embed_url
+        });
+      });
+  };
+
   render() {
     return (
       <RandomComponent>
-        <Header>Sem tempo irmão?</Header>
-        <RandomButton>Randomize</RandomButton>
+        <RandomButton onClick={this.getGif}>Randomize</RandomButton>
+        <iframe
+          title="gif"
+          src={this.state.embedGif}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
       </RandomComponent>
     );
   }
---
+++ src/credentials.js
@@ -0,0 +1,3 @@
+//Put your giphy credential here
+const credential = "your credential here";
+export default credential;
